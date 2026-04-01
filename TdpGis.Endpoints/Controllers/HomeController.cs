c812e2d4514d443800b8314a4c20d139059a1309
Bob Pham <bobpham.tdp@gmail.com>
2026-04-01 23:37:45 +1100

    update admin UI

--- TdpGis.Endpoints/Controllers/HomeController.cs
+++ TdpGis.Endpoints/Controllers/HomeController.cs
@@ -10,11 +10,7 @@ namespace TdpGis.Endpoints.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+     
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
