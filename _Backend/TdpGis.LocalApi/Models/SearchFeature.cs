0384f4f4ee02448efbb861c0548d9b72325f8b3e
Bob Pham <bobpham.tdp@gmail.com>
2026-05-12 00:50:16 +1000

    update code

--- _Backend/TdpGis.LocalApi/Models/SearchFeature.cs
+++ _Backend/TdpGis.LocalApi/Models/SearchFeature.cs
@@ -2,20 +2,6 @@
 
 public class SearchFeature
 {
-    // Parameterless constructor required by ConfigurationBinder / IOptions binding
-    public SearchFeature()
-    {
-    }
-
-    // Optional convenience constructor
-    public SearchFeature(string connectionString, string collectionName, string databaseName, string searchField)
-    {
-        ConnectionString = connectionString;
-        CollectionName = collectionName;
-        DatabaseName = databaseName;
-        SearchField = searchField;
-    }
-
     public required string ConnectionString { get; set; }
     public required string CollectionName { get; set; }
     public required string DatabaseName { get; set; }
